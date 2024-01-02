4b7c3aff0c765c1ba1a2c173cdb79df52d9cd77a
Tuğsan <tugsancitir@hotmail.com>
2024-01-02 20:07:53 +0300

    Connect a player hit detection signal to world

--- scripts/world.gd
+++ scripts/world.gd
@@ -3,9 +3,11 @@ extends Node2D
 var Pillar : PackedScene = preload("res://scenes/pillars.tscn")
 
 @onready var timer : Node = $PillarTimer
+@onready var player : Node = $Plane/Area2D
 
 func _ready():
 	randomize()
+	player.body_entered.connect(player_hits_obstacle)
 	spawn_pillar()
 
 
@@ -18,3 +20,6 @@ func _on_pillar_timer_timeout():
 	add_child(pillar_instance)
 	timer.wait_time = randf_range(1.5, 4.0)
 	timer.start()
+
+func player_hits_obstacle(body):
+	print("Hit Detected")
