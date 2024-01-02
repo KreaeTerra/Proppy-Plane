2a4173e6afa0588115610269dc748e3842803ba6
Tuğsan <tugsancitir@hotmail.com>
2024-01-02 13:13:12 +0300

    Create left/right movement for the player plane

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -1,1 +1,8 @@
 extends CharacterBody2D
+
+@export var speed : int = 160
+
+func _process(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+	move_and_slide()
