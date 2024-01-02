7738b283c0fbc3724d12136fe48938fcce8d98f4
Tuğsan <tugsancitir@hotmail.com>
2024-01-02 13:24:14 +0300

    Add a gravity and a terminal velocity to the player

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
 @export var speed : int = 160
+@export var gravity : int = 10
+@export var terminal_velocity : int = 400
 
 func _process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * speed
+	print("vertical speed " , velocity.y)
+	if velocity.y < terminal_velocity:
+		velocity.y += gravity
 	move_and_slide()
