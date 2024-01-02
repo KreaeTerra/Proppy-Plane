734b8d91ea909e15fc87f43796c03ba936dcfec3
Tuğsan <tugsancitir@hotmail.com>
2024-01-02 13:31:34 +0300

    Add the ability to lift to the player and set some limits

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -4,8 +4,13 @@ extends CharacterBody2D
 @export var gravity : int = 10
 @export var terminal_velocity : int = 400
 
+@export var lift_force : int = -30
+@export var maximum_lift_speed : int = -100
+
 func _process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_pressed("ui_up") and velocity.y > maximum_lift_speed:
+		velocity.y += lift_force
 	velocity.x = direction * speed
 	print("vertical speed " , velocity.y)
 	if velocity.y < terminal_velocity:
