0e3058ef7cae69cc024339ab577aac1733667066
Tuğsan <tugsancitir@hotmail.com>
2024-01-03 13:23:22 +0300

    Make the reload prompt work by unpausing and reloading the World

--- scripts/game_over_screen.gd
+++ scripts/game_over_screen.gd
@@ -6,3 +6,8 @@ var score : int = 0
 func _ready():
 	score = Gamedata.score
 	score_label.text = "YOU MANAGED TO GO THROUGH " + str(score) + " OBSTACLES"
+
+func _input(event):
+	if event.is_action_pressed("ui_restart"):
+		get_tree().paused = false
+		get_tree().reload_current_scene()
